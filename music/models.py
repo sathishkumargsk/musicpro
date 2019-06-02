331a86398f469075371330ec15682ad176a968a1
sathishkumargsk <sathishkumar1069@gmail.com>
2019-06-02 11:52:56 +0530

    Creating Artist Album Song Model for music portal

--- music/models.py
+++ music/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+	artist_name = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.artist_name
+
+class Album(models.Model):
+	artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+	album_title = models.CharField(max_length=250)
+	genre = models.CharField(max_length=100)
+	album_cover_image = models.FileField()
+
+	def __str__(self):
+		return self.album_title
+
+class Song(models.Model):
+	artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
+	song_title = models.CharField(max_length=250)
+	song_file = models.FileField()
+
+	def __str__(self):
+		return self.song_title 
+
+		
+
