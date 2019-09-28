e56188e0a34b3b0f53ef6228aa6f911815fbb824
sathish <sathishkumar1069@gmail.com>
2019-09-28 19:17:33 +0530

    Before import changing mp3 meta tags names and import

--- music/admin.py
+++ music/admin.py
@@ -1,8 +1,85 @@
-from import_export import fields, resources
+from import_export import fields, resources, widgets
 from import_export.widgets import ForeignKeyWidget
 from django.contrib import admin
 from .models import Artist, Album, Song
+from urllib import request
+from django.conf import settings
 from import_export.admin import ImportExportModelAdmin, ImportMixin, ExportMixin, ImportExportMixin
+import pprint
+import logging
+import os
+import eyed3
+
+
+media_folder = "media/"
+"""
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/tmp/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+})
+
+# This retrieves a Python logging instance (or creates it)
+logger = logging.getLogger(__name__)
+
+"""
+
+def before_import_row(self,row, **kwargs):
+    mp3file = row['song_file']
+    mp3filename = mp3file.rsplit('/', 1)
+    filename = row['song_title']
+    sitename = "Newtamilsongs.In"
+    extension = ".mp3"
+    newfilename = filename+"-"+sitename+extension
+    mp3filepath = media_folder+mp3file
+    new_song_file = "mp3/"+newfilename
+    newfilenamepath = media_folder+"mp3/"+newfilename
+    coverimage = Album.objects.get(album_title=row['album'])
+    albumcover = coverimage.album_cover_image.url;
+    albumcoverpath = albumcover[1:]
+    if os.path.isfile(mp3filepath):
+        mp3filepathchecked = mp3filepath
+    else:
+        mp3filepathchecked = newfilenamepath
+    audiofile  = eyed3.load(mp3filepathchecked)
+    audiofile.tag.title = row['song_title']
+    audiofile.tag.artist = row['artist']
+    audiofile.tag.album = row['album']
+    audiofile.tag.images.remove('')
+    imagedata = open(albumcoverpath,"rb").read()
+    # append image to tags
+    audiofile.tag.images.set(3,imagedata,"image/jpeg")
+    audiofile.tag.save()
+    if os.path.isfile(mp3filepath):
+        os.rename(mp3filepath,newfilenamepath)
+    row['song_file'] = new_song_file # update value to id ob new object
+
+resources.Resource.before_import_row = before_import_row
 
 
 class ArtistResource(resources.ModelResource):
